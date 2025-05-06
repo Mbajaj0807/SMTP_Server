4355fdec6a997ed2499e54371ded401a01ca765f
unknown <e23cseu0624@bennett.edu.in>
2025-05-06 14:19:20 +0530

    Added readline

--- sendmail.js
+++ sendmail.js
@@ -1,23 +1,36 @@
 const nodemailer = require('nodemailer');
-//Update
-const transporter = nodemailer.createTransport({
-  host: '54.227.121.116',
-  port: 2525,
-  secure: false,
-  tls: {
-    rejectUnauthorized: false
-  }
+const readline = require('readline');
+
+// Create readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-transporter.sendMail({
-  from: 'manan.losthub.xyz',
-  to: 'mananbajaj0807@gmail.com',
-  subject: 'SMTP Test',
-  text: 'This is a test email from local SMTP server'
-}, (err, info) => {
-  if (err) {
-    console.error('Error sending mail:', err);
-  } else {
-    console.log('Mail sent:', info);
-  }
+// Prompt for the 'to' email address
+rl.question('Please enter the recipient email address: ', (toEmail) => {
+  // Create transporter
+  const transporter = nodemailer.createTransport({
+    host: '54.227.121.116',
+    port: 2525,
+    secure: false,
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  // Send email
+  transporter.sendMail({
+    from: 'manan.losthub.xyz',
+    to: toEmail,
+    subject: 'SMTP Test',
+    text: 'This is a test email from local SMTP server'
+  }, (err, info) => {
+    if (err) {
+      console.error('Error sending mail:', err);
+    } else {
+      console.log('Mail sent:', info);
+    }
+    rl.close(); // Close the readline interface
+  });
 });
